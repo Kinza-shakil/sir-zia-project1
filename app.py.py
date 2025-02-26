0dd13ff30fac26b1c01d6a6094f8c0c3c5d6a6c3
Kinza-shakil <creativivity.mind@gmail.com>
2025-02-26 12:07:31 +0500

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -45,7 +45,7 @@ st.subheader("Data Cleaning Options")
 if st.checkbox(f"clean data for {file.name}"):
     col1, col2 = st.columns(2)
     
- with col1:
+with col1:
  if st.button(f"Remove duplicates from the file : {file.name}"):
  df.drop_duplicates(inplace=True)
  st.write("Duplicates removed!")
